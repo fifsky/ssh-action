fe5bcf5433b86638fc68620fd957e10bcd47bd66
fifsky <fifsky@gmail.com>
2019-09-03 18:58:46 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 # Exit on error. Append "|| true" if you expect an error.
 set -o errexit  # same as -e
-# Exit on error inside any functions or subshells.
-set -o errtrace
 # Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
 set -o nounset
 # Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
