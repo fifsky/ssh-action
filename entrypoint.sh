cc7b6b8603a118ec35815f8686f528d02b31281b
fifsky <fifsky@gmail.com>
2019-09-03 19:17:11 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-# Exit on error. Append "|| true" if you expect an error.
-set -o errexit  # same as -e
-# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
-set -o nounset
-# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
-set -o pipefail
+set -eu
 
 printf '\033[33m Warning: This action does not currently support host verification; verification is disabled. \n \033[0m\n'
 
