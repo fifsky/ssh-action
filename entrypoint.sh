58b3c484be9c20cf118fd3b939a6d2cb3c769512
Xudong Cai <fifsky@gmail.com>
2021-08-24 21:59:49 +0800

    fix exit failed

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ chmod 600 "$SSHPATH/known_hosts"
 chmod 600 "$SSHPATH/deploy_key"
 
 echo "$INPUT_COMMAND" > $HOME/shell.sh
-echo "exit $?" >> $HOME/shell.sh
+echo "exit \$?" >> $HOME/shell.sh
 cat $HOME/shell.sh
 
 echo Start Run Command
